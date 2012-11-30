26d40dbb335b87cb761b4a43a52ccf6aeec36879
Piedone <piedone@pyrocenter.hu>
2012-11-30 23:26:49 +0100

    Adding preprocessor

---
+++ CoffeeScriptPreprocessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using CoffeeSharp;
+using Piedone.Combinator.EventHandlers;
+
+namespace Piedone.Combinator.CoffeeScript {
+    public class CoffeeScriptPreprocessor : ICombinatorResourceEventHandler
+    {
+
+        public void OnContentLoaded(Models.CombinatorResource resource)
+        {
+            if (Path.GetExtension(resource.AbsoluteUrl.ToString()).ToLowerInvariant() != ".coffee") return;
+
+            resource.Content = new CoffeeScriptEngine().Compile(resource.Content);
+        }
+
+        public void OnContentProcessed(Models.CombinatorResource resource)
+        {
+        }
+    }
+}
